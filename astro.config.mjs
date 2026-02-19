39286640087a34f0b936f63a5bf9147e190d2018
sagi <sagi@eretz.org.il>
2026-02-19 18:12:44 +0200

    first

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://niro-prod.github.io',
+  output: 'static',
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
